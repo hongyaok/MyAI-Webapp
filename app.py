0aa8dc8c467fe2ae06afac6ce138ef59afff8e09
hong yaok <hongyaok@gmail.com>
2024-10-12 23:10:41 +0800

    Fixed error in app.py

--- app.py
+++ app.py
@@ -81,9 +81,6 @@ def creators_gallery():
 def gall_file(filename):
     return send_from_directory(IMAGE_FOLDER, filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory('uploads', filename)
